dcc09f395050eaad7b596b7619258b8c7549a31f
Jonas Wagner <jonas@29a.ch>
2016-03-13 17:50:49 +0100

    moved debounce dependency,release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
-echo $1
+version="$1"
+if [ -z "$version" ]; then
+    echo "Usage: release.sh version"
+    exit
+fi
+
+echo $version
 git checkout release
-git tag -a $1
-git push origin $1
+node -e "p=require('./package.json');p.version='$version';\
+require('fs').writeFileSync('package.json', JSON.stringify(p, undefined, '  '));"
+node -e "p=require('./bower.json');p.version='$version';\
+require('fs').writeFileSync('bower.json', JSON.stringify(p, undefined, '  '));"
+gulp zip dist
+git diff
+echo "Confirm release with YES"
+read confirmation
+if [ "$confirmation" != 'YES' ]; then
+    echo "Ok, maybe not."
+    exit
+fi
+gulp release
+git tag -a "v$version"
+git push origin "v$version"
+npm release
 g checkout master
